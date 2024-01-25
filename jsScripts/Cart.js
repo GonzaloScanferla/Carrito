db0fd3ff31f7c21bca032a4af30212d560d2c9a8
Gonzalo Scanferla <gonzalo.scanferla@gmail.com>
2024-01-25 12:48:07 +0100

    Añadido método cart.update
    Pendiente revisar totalCart

--- jsScripts/Cart.js
+++ jsScripts/Cart.js
@@ -1,46 +1,50 @@
 class Cart {
   #currency;
-  #products
-  #cart
-  constructor (data) {
-    this.#currency = data.currency
-    this.#products = data.products
-    this.#cart = []
+  #products;
+  #cart;
+  constructor(data) {
+    this.#currency = data.currency;
+    this.#products = data.products;
+    this.#cart = [];
   }
   init() {
     this.#products.forEach((product) => {
-      this.#cart.push({ SKU: product.SKU, qty: 0, total: 0 });
+      const { SKU, title, price } = product;
+      this.#cart.push({
+        SKU: SKU,
+        title: title,
+        price: price,
+        qty: 0,
+        total: 0,
+      });
     });
   }
   returnCart() {
-      return this.#cart;
+    return this.#cart;
+  }
+
+  returnCartProduct(SKU) {
+    return this.#cart.find((product) => product.SKU == SKU);
+  }
+
+  update(SKU, operation) {
+    const currentProduct = this.returnCartProduct(SKU);
+    if (operation === "add") {
+      currentProduct.qty = currentProduct.qty + 1;
+    } else if (operation === "remove") {
+      currentProduct.qty = currentProduct.qty > 0 ? currentProduct.qty - 1 : 0;
     }
-  
-  returnCartProduct (SKU) {
-    return this.#cart.find (product => product.SKU == SKU );
-    }
+    currentProduct.total = (currentProduct.qty * currentProduct.price).toFixed(
+      2
+    );
+    return currentProduct.qty;
+  }
+
+  totalCart() {
+    let totalCart = 0;
+    this.#cart.forEach((product) => {
+      totalCart = (Number(totalCart) + Number(product.total)).toFixed(2);
+    });
+    return totalCart;
+  }
 }
-
-
-
-
-// update(SKU, operation) {
-//   const currentProduct = this.#cart.find((product) => SKU == product.SKU);
-//   if (operation === "add") {
-//     currentProduct.qty = currentProduct.qty + 1;
-//   } else if (operation === "remove") {
-//     currentProduct.qty = currentProduct.qty > 0 ? currentProduct.qty - 1 : 0;
-//   }
-//   currentProduct.total =
-//     (currentProduct.qty * productInfo(SKU).price).toFixed (2);
-//   return currentProduct.qty;
-// }
-
-// totalCart() {
-//   let totalCart = 0 
-//   this.#cart.forEach (product => {
-
-//     totalCart = (Number(totalCart) + Number(product.total)).toFixed (2)
-//     })
-//   return totalCart
-// }
